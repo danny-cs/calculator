94c9b228762c9c8891ce236ce27c650c9817020f
Danny Nguyen <danny_nguyen1@student.uml.edu>
2016-11-03 22:27:58 -0400

    added comments to files.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # HW6 Calculator
 A simple multiplication table built using HTML and Javascript
 
+Descriptions are added in ech individual file describing what they do and what was added.
 
 
 # How to push changes
--- calculator.js
+++ calculator.js
@@ -1,5 +1,20 @@
+/*
+    -Danny Nguyen
+    -danny_nguyen1@student.uml.edu
+    -University Of Massachusetts Lowell - 96.161 GUI Programming 1
+    -Last updated November 3rd, 2016
+*/
+
+/*  This calculator.js file implements the actual computation needed for the multiplication table
+    by creating the table and populating it dynamically based on the numbers which the user inputs. 
+    This code also take into account edge cases such as when the user enters something other than 
+    a number where it will have an alert pop up asking for valid inputs. It also tests for the case 
+    where the user enters invalid range for number 2 and number 4.
+*/
+
 function calculate() {
     $("table").children().remove();
+    //creates the place holder for the numbers
     var num1 = Number(document.forms["calculatorForm"]["num1"].value);
     var num2 = Number(document.forms["calculatorForm"]["num2"].value);
     var num3 = Number(document.forms["calculatorForm"]["num3"].value);
@@ -12,21 +27,24 @@ function calculate() {
             alert("Number 2 has to be greater than Number 1 AND Number 4 has to be greater than Number 3.");
         } else {
             var table = document.getElementById("timesTable");
-            
-            var firstRow = table.insertRow(0); // First row header
-            firstRow.insertCell(0); // Empty cell
-            for (var j = 0; j <= (num2 - num1); j++) {
-                var cell = firstRow.insertCell(j + 1); // Add header titles
-                cell.innerHTML ="<p>" + (j + (num1)) + "</p>";
-            }
 
+            // First row header
+            var firstRow = table.insertRow(0);
+            // Empty cell
+            firstRow.insertCell(0);
+            for (var j = 0; j <= (num2 - num1); j++) {
+                // Add header titles
+                var cell = firstRow.insertCell(j + 1);
+                cell.innerHTML = "<p>" + (j + (num1)) + "</p>";
+            }
             for (var i = 0; i <= (num4 - num3); i++) {
                 var row = table.insertRow(i + 1);
-                var firstCell = row.insertCell(0); // Add row title as first cell
-                firstCell.innerHTML ="<p>" + (i + num3) + "</p>";
+                // Add row title as first cell
+                var firstCell = row.insertCell(0);
+                firstCell.innerHTML = "<p>" + (i + num3) + "</p>";
                 for (var j = 0; j <= (num2 - num1); j++) {
                     var cell = row.insertCell(j + 1);
-                    cell.innerHTML ="<p>" + ((num3 + i) * (num1+j)) + "</p>";
+                    cell.innerHTML = "<p>" + ((num3 + i) * (num1 + j)) + "</p>";
                 }
             }
         }
